663bec171698c15be9867c991eff84eb93cfe70b
naimuru <nbello@unal.edu.co>
2024-04-02 07:43:27 -0500

    Modified definition of create

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,25 +1,23 @@
 class Users::SessionsController < Devise::SessionsController
-    
-    before_action :drop_session_cookie
+  before_action :drop_session_cookie
+  respond_to :json
 
-    private
-
-    def drop_session_cookie
-      request.session_options[:skip] = true
-    end
-
-    respond_to :json
-  
-    def create
-      super do |resource|
-        render json: { message: 'Signed in successfully.', user: resource }, status: :created
-      end
-    end
-  
-    def destroy
-      super do
-        render json: { message: 'Signed out successfully.' }, status: :ok
-      end
-    end
+  # POST /api/v1/sign_in
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    sign_in(resource_name, resource) if resource.persisted?
+    render json: { message: 'Signed in successfully.', user: resource }, status: :created
   end
-  
+
+  # DELETE /api/v1/sign_out
+  def destroy
+    sign_out
+    render json: { message: 'Signed out successfully.' }, status: :ok
+  end
+
+  private
+
+  def drop_session_cookie
+    request.session_options[:skip] = true
+  end
+end
