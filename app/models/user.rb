4c74a3c01bb62c8d62e50cfb2b245125f9a41df1
naimuru <nbello@unal.edu.co>
2024-04-02 02:29:04 -0500

    added jwt auth and jwt revocation along with some loggers to see the JWT token dispatched after a commit on a DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,19 @@
 class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: Denylist
 
-  # Validations for the new fields
-  #validates :emailAddr, presence: true, uniqueness: true
-  # validates :userName, presence: true, uniqueness: true
-  #validates :keyIdAuth, presence: true
-  
-  # nickname is optional, so no validation for presence
+  def on_jwt_dispatch(token, payload)
+  log_jwt_dispatch(token, payload)
+  end
+
+  private
+
+  # Example method to log the token and payload
+  def log_jwt_dispatch(token, payload)
+    Rails.logger.info "JWT Token dispatched: #{token}"
+    Rails.logger.info "Payload: #{payload.inspect}"
+  end
+
 end
